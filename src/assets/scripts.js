c8fc34eb6741843f7a7fe1eeea743bd41efde2da
brendanoneildev <btayvideo@gmail.com>
2018-04-13 16:49:43 -0700

    Added a persistent header as well as a script to expand sidebar.

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -1,1 +1,8 @@
-var navbar = document.querySelector("nav");
+var animated = document.querySelectorAll(".sidebar");
+var sidebarButton = document.querySelector(".expand-button");
+
+sidebarButton.addEventListener("click", function(){
+    for (var i = 0; i < animated.length; i++) {
+        animated[i].classList.toggle("sidebar-open");
+    }
+});
