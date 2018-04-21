ff269439f5d6b23c9a10b328037194fc96ba2adf
brendanoneildev <btayvideo@gmail.com>
2018-04-20 18:05:08 -0700

    Changed to linear gradient and fixed animations

---
+++ assets/scripts.js
@@ -0,0 +1,73 @@
+var animated = document.querySelectorAll(".sidebar");
+var sidebarButton = document.querySelector(".expand-button");
+var anchor = document.querySelectorAll("nav a");
+
+var hasScrolled = false;
+var prevScrollPos = window.pageYOffset;
+var minScroll = 15;
+
+sidebarButton.addEventListener("click", function() {
+    for(var i = 0; i < animated.length; i++) {
+        animated[i].classList.toggle("sidebar-open");
+    }
+});
+
+window.onscroll = function() {
+    hasScrolled = true;
+};
+
+// for(var i = 0; i < anchor.length; i++) {
+//     anchor[i].addEventListener("click", function() {
+//         hideSidebar();
+//         setTimeout(function() {
+//             document.querySelector("nav").classList.add("navbar-up");
+//         }, 251);
+//     });
+// };
+
+setInterval(function() {
+    if (hasScrolled) {
+        hideNavbar();
+        hasScrolled = false;
+    }
+}, 250);
+
+function hideNavbar() {
+    var newScrollPos = window.pageYOffset;
+
+    if(Math.abs(prevScrollPos-newScrollPos) <= minScroll || newScrollPos < document.querySelector("header").offsetHeight) {
+        if(newScrollPos < document.querySelector("header").offsetHeight) {
+            document.querySelector("nav").classList.remove("navbar-up");
+        }
+        return;
+    }
+
+    if(newScrollPos > prevScrollPos) {
+        document.querySelector("nav").classList.add("navbar-up");
+    } else {
+        document.querySelector("nav").classList.remove("navbar-up");
+    }
+    prevScrollPos = newScrollPos;
+};
+
+function hideSidebar() {
+    for(var i = 0; i < animated.length; i++) {
+        animated[i].classList.toggle("sidebar-open");
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        hideSidebar();
+
+        setTimeout(function() {
+            document.querySelector("nav").classList.add("navbar-up");
+        }, 1000);
+    });
+});
